218d28206d3612471b60e8990b2a37909bca1a50
freedom00000 <qblr11@gmail.com>
2023-05-11 16:38:24 +0300

    - add avg speed for hola - add requirements

--- hola.py
+++ hola.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import statistics
 import serial
 
 from utils import thread
@@ -12,9 +12,19 @@ class Hola:
 
     def __init__(self):
         self.hola = serial.Serial(port=self.PORT, baudrate=self.BAUD_RATE)
-        self.speed = 0
+        self._speed = [0] * 5
+        self._speed_cnt = 0
         self.speed_no_changes_cnt = 0
         self.prev_time = datetime.now()
+
+    @property
+    def speed(self):
+        return round(statistics.fmean(self._speed), 1)
+
+    @speed.setter
+    def speed(self, value):
+        self._speed[self._speed_cnt] = value
+        self._speed_cnt = self._speed_cnt + 1 if self._speed_cnt < len(self._speed) - 1 else 0
 
     @thread
     def __listener_loop(self, event):
@@ -25,7 +35,7 @@ class Hola:
                 data = self.hola.read(size)
                 if data == b'#':
                     now = datetime.now()
-                    self.speed = round(500.0 / (now - self.prev_time).total_seconds(), 1)
+                    self.speed = 500.0 / (now - self.prev_time).total_seconds()
                     self.prev_time = now
                     self.speed_no_changes_cnt = 0
                     event()
@@ -34,13 +44,14 @@ class Hola:
                 if self.speed_no_changes_cnt > self.MAX_SPEED_NO_CHANGES:
                     self.speed = 0
 
-
     def start(self, event):
         self.__listener_loop(event)
 
     def stop(self):
+        self.hola.write(b'a')
         self.hola.close()
 
 
 if __name__ == "__main__":
     hola = Hola()
+    hola.start()
